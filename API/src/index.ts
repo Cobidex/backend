cd8f5ec2e9bfe0f94865a2886707c2907f42dcd5
Stephen <stevobialor@gmail.com~>
2024-04-29 19:55:24 +0100

    added routes

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import cors from 'cors';
 import dotenv from "dotenv";
+import { appointmentRoutes, diagnosisRoute, patientRoute, userRoutes } from "./routes";
 
 const app = express();
 
@@ -9,6 +10,11 @@ app.use(cors());
 app.use(express.json())
 
 const PORT = process.env.PORT;
+
+app.use('/api/diagnosis', diagnosisRoute);
+app.use('api/users', userRoutes);
+app.use('/api/patients', patientRoute);
+app.use('api/appointments', appointmentRoutes);
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
---
+++ API/src/routes/index.ts
@@ -0,0 +1,6 @@
+import patientRoute from "./patient";
+import diagnosisRoute from "./diagnosis";
+import userRoutes from  "./users";
+import appointmentRoutes from "./appointment";
+
+export { patientRoute, diagnosisRoute, userRoutes, appointmentRoutes };
